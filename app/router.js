352357beb1fc13526ba088f894552ac4c91280e0
wrumly <wrumly@gmail.com>
2015-04-15 00:48:54 +0300

    lesson #1 prepares

--- app/router.js
+++ app/router.js
@@ -4,29 +4,29 @@ define(
     function(Backbone) {
         var Controller = Backbone.Router.extend({
             routes: {
-                "": "start", // Пустой hash-тэг
-                "!/": "start", // Начальная страница
-                "!/page1": "page1", // Блок удачи
-                "!/page2": "page2" // Блок ошибки
+                "": "home",
+                "home": "home",
+                "lesson1": "lesson1",
+                "lesson2": "lesson2"
             },
 
-            start: function () {
-                $(".g-view").addClass('hidden'); // Прячем все блоки
-                $("#page0").removeClass('hidden'); // Показываем нужный
+            home: function () {
+                $(".g-view").addClass('hidden');
+                $("#home").removeClass('hidden');
                 $('nav li').removeClass('active');
                 $('nav li a[href=\"' + location.hash + '\"]').closest('li').addClass('active');
             },
 
-            page1: function () {
+            lesson1: function () {
                 $(".g-view").addClass('hidden');
-                $("#page1").removeClass('hidden');
+                $("#lesson1").removeClass('hidden');
                 $('nav li').removeClass('active');
                 $('nav li a[href=\"' + location.hash + '\"]').closest('li').addClass('active');
             },
 
-            page2: function () {
+            lesson2: function () {
                 $(".g-view").addClass('hidden');
-                $("#page2").removeClass('hidden');
+                $("#lesson2").removeClass('hidden');
                 $('nav li').removeClass('active');
                 $('nav li a[href=\"' + location.hash + '\"]').closest('li').addClass('active');
             }
