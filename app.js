c21f63f64b830f251b217e1d47af3b255d3380fe
Vladimir Kalinichev <vladimir.kalinichev@kaspersky.com>
2015-04-17 19:04:43 +0300

    [github.io] libs moved to cdn

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ requirejs.config({
     "baseUrl": "app",
     "paths": {
         "app": ".",
-        "jquery":    "../bower_components/jquery/dist/jquery",
-        "backbone":  "../bower_components/backbone/backbone",
-        "underscore":  "../bower_components/underscore/underscore",
-        "bootstrap": "../bower_components/bootstrap/dist/js/bootstrap"
+        "jquery":     "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min",
+        "backbone":   "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
+        "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min",
+        "bootstrap":  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min"
     },
     shim: {
         "bootstrap": {
