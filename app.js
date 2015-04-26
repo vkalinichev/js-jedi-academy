b9605cee7e7d6924ed1ddc7b0fd5c26ed9aeee25
Vladimir Kalinichev <vladimir.kalinichev@kaspersky.com>
2015-04-26 23:17:15 +0300

    [lesson #2] fucking windows filenames

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ requirejs.config({
         "backbone":   "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
         "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min",
         "bootstrap":  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
-        "jquery.turtlesearcher": "lessons/lesson2/jquery.turtlesearcher"
+        "jquery.turtlesearcher": "lessons/lesson2/jquery.turtle-searcher"
     },
     shim: {
         "bootstrap": {
